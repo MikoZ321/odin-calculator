b5808279fa7442d3ce592756edbba5c86f6eeab5
MikoZ321 <0123456789miko@gmail.com>
2026-04-29 17:50:13 +0200

    add divide

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ function multiply(a, b) {
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     return a * b;
 }
+// Divides two values and returns the output
+function divide(a, b) {
+    if (typeof a != "number" || typeof b != "number") throw TypeError;
+    if (b == 0) throw RangeError;
+    return a / b;
+}
