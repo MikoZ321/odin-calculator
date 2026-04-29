a0003379586119f48fb20bb6172e93d3bcdf1ce2
MikoZ321 <0123456789miko@gmail.com>
2026-04-29 17:46:27 +0200

    add multiply

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// Adds two integer values and returns the output
+// Adds two values and returns the output
 function add(a, b) {
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     return a + b;
 }
 
-// Subtracts two integer values and returns the output
+// Subtracts two values and returns the output
 function subtract(a, b) {
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     return a - b;
+}
+
+// Multiplies two values and returns the output
+function multiply(a, b) {
+    if (typeof a != "number" || typeof b != "number") throw TypeError;
+    return a * b;
 }
