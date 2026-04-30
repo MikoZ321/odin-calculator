a0408ca19190409d34a2515e89cf51a5b9c5ebd2
MikoZ321 <0123456789miko@gmail.com>
2026-04-30 08:45:42 +0200

    add basic operate

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const ALLOWED_OPERATORS = ["+", "-", "*", "/"];
 
-// Adds two values and returns the output
+// Define system variables
+let number_buffer = "";
+let operator_buffer = "";
+let registers = [undefined, undefined];
+let current_register_index = 0;
+
 function add(a, b) {
+// Add two values, return result
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     return a + b;
 }
 
-// Subtracts two values and returns the output
 function subtract(a, b) {
+// Subtract two values, return result
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     return a - b;
 }
 
-// Multiplies two values and returns the output
 function multiply(a, b) {
+// Multiply two values, return result
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     return a * b;
 }
-// Divides two values and returns the output
+
 function divide(a, b) {
+// Divide two values, return result
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     if (b == 0) throw RangeError;
     return a / b;
 }
 
-// Handles the operation logic, returns the result
 function operate(a, b, operator) {
+// Handle operation logic, return result
+    const ALLOWED_OPERATORS = ["+", "-", "*", "/"];
+
     if (typeof a != "number" || typeof b != "number") throw TypeError;
     if (typeof operator != "string" && !(operator in ALLOWED_OPERATORS)) throw TypeError;
 
@@ -36,24 +44,68 @@ function operate(a, b, operator) {
         case "/": return divide(a, b);
     }
 }
-// Typing in numbers
+
+// Input numbers
 // TODO: account for overflow
-const display_div = document.querySelector("#display");
-let display_buffer = "";
+const number_display = document.querySelector("#number-display");
 
 const number_buttons = document.querySelectorAll(".number-button");
 number_buttons.forEach((button) => {
     button.addEventListener("click", () => {
         let button_number = button.getAttribute("id");
-        display_buffer += button_number;
-        display_div.innerHTML = display_buffer; 
+        number_buffer += button_number;
+        number_display.innerHTML = number_buffer; 
     });
 });
 
-// Clear display
-// TODO: clear memory
+// Handle All-Clear (AC) button
+function clear(display) {
+    // Clear provided display, return empty string
+    display.innerHTML = "";
+    return "";
+}
 const clear_button = document.querySelector("#clear");
 clear_button.addEventListener("click", () => {
-    display_div.innerHTML = "";
-    display_buffer = "";
+    number_buffer = clear(number_display);
+    operator_buffer = clear(operator_display);
+
+    // Clear memory
+    registers.map(() => (""));
+    current_register_index = 0;
+})
+
+// Input operator
+const operator_display = document.querySelector("#operator-display");
+
+const operator_buttons = document.querySelectorAll(".operator-button");
+operator_buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        // Allow operator change if no second number input yet
+        if (number_buffer.length == 0 || current_register_index == 0) {
+            operator_buffer = button.getAttribute("id");
+            operator_display.innerHTML = operator_buffer;
+        }
+
+        if (current_register_index == 0) {
+            registers[current_register_index] = Number(number_buffer);
+            current_register_index = 1;
+            number_buffer = clear(number_display);
+        }
+    });
+});
+
+// Compute operation
+const equals_button = document.querySelector("#equals");
+equals_button.addEventListener("click", () => {
+    console.log(registers, current_register_index, number_buffer, operator_buffer);
+    if (registers[0] == undefined || number_buffer.length == 0 || operator_buffer.length == 0) throw RangeError;
+
+    registers[current_register_index] = Number(number_buffer);
+    number_buffer = clear(number_display);
+
+    registers[0] = Number(operate(registers[0], registers[1], operator_buffer));
+    operator_buffer = clear(operator_display);
+    registers[1] = undefined;
+
+    number_display.innerHTML = registers[0];
 })
